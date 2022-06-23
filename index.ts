dad5ccb5cf0f2bc61038332313b87a502a42dc5e
joao vitor <developer.joaovitor@gmail.com>
2022-06-23 20:44:56 -0300

    nos codigos no index.ts

--- index.ts
+++ index.ts
@@ -4,31 +4,67 @@ function soma(a: number, b: number){
 
 console.log(soma(1, 2));
 
-//types
-
-//interfaces
-
+//types & interfaces
 interface IAnimal{
     nome: string;
     tipo: 'terrestre' | 'aquatico';
-    executarRugido(alturaEmDecibeis: number): void;
+    domestico: boolean;
 }
 
 interface IFelino extends IAnimal{
     visaoNoturna: boolean;
 }
 
-const animal: IAnimal = {
-    nome: 'Elefante',
-    tipo: 'terrestre',
-    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+interface ICanino extends IAnimal{
+    porte: 'pequeno' | 'medio' | 'grande';
 }
 
-console.log(animal.executarRugido(1));
+type IDomestico = IFelino | ICanino;
+
+const animal: IDomestico = {
+   domestico: true,
+   nome: 'cachorro',
+   porte: 'medio',
+   tipo: 'terrestre'
+}
 
 const felino: IFelino = {
     nome: 'Leao',
     tipo:  'terrestre',
     visaoNoturna: true,
-    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}dB`)
+    domestico: false
+}
+
+//inputs
+
+const input = document.getElementById('input') as HTMLInputElement;
+
+input.addEventListener('input', (event) => {
+    console.log('Digitei');
+    const i = event.currentTarget as HTMLInputElement;
+    console.log(i.value);
+});
+
+//generic types
+
+function adicionaApendiceALista<T>(array: any[], valor: T){
+    return array.map(item => item + valor);
+}
+
+console.log(adicionaApendiceALista([1, 2, 3], 1));
+
+interface IUsuario {
+    id: string;
+    email: string;
+    cargo?: 'funcionario' | 'gerente' | 'coordenador' | 'supervisor';//botando o ? transforma o intem em um dado opcional
+}
+
+function redirecione(usuario: IUsuario){
+
+    if(usuario.cargo){
+        //redirecionar(usario.cargo);
+    }
+
+    //redirecionar para a area do usuario
+
 }
